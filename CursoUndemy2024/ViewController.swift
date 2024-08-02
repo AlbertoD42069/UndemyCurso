3fe933c0c33b57c390050964b72ee46ff532e7df
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-08-02 17:00:29 -0600

    Controles y vistas de texto

--- CursoUndemy2024/ViewController.swift
+++ CursoUndemy2024/ViewController.swift
@@ -7,14 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITabBarController {
+    
+    let vcControlsAndViews = ControlsAndViewsViewController()
+    let vcVistasCurso = VistasViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("hola mundo")
+        tabBar.backgroundColor = .white
+        
+        vcControlsAndViews.navigationItem.largeTitleDisplayMode = .automatic
+        vcVistasCurso.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let vcControlNav = UINavigationController(rootViewController: vcControlsAndViews)
+        let vcVistasNav = UINavigationController(rootViewController: vcVistasCurso)
+        
+        
+        
+        let vcImage = UIImage(systemName: "menucard.fill")
+        let vcVistaImagen = UIImage(systemName: "location.circle.fill")
+        
+        vcControlNav.tabBarItem  = UITabBarItem(title: "Controles", image: vcImage, tag: 1)
+        vcVistasNav.tabBarItem = UITabBarItem(title: "Vistas", image: vcVistaImagen, tag: 2)
+        
+        let items = [vcControlNav, vcVistasNav]
+        
+        for nav in items {
+            tabBar.barTintColor = .darkGray
+        }
+        setViewControllers(items, animated: true)
     }
-
-
 }
-
