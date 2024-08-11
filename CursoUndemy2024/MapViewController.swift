ec236981bd0c6d161817fc885713b88cf3641493
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-08-10 19:47:37 -0600

    ClaseMapView

--- CursoUndemy2024/MapViewController.swift
+++ CursoUndemy2024/MapViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
-
+import MapKit
 class MapViewController: UIViewController {
+        
+    private let myMap: MKMapView = {
+        let map : MKMapView = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //17.97218171180922, -92.93213372609293
+        myMap.centerCoordinate = CLLocationCoordinate2D(latitude: 17.97218171180922, longitude: -92.93213372609293)
+        view.addSubview(myMap)
+        title = "Controlador"
+        
+        NSLayoutConstraint.activate([
+            myMap.safeAreaLayoutGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            myMap.safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            myMap.safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            myMap.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
 
+        ])
         // Do any additional setup after loading the view.
     }
     
--- CursoUndemy2024/ViewController.swift
+++ CursoUndemy2024/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UITabBarController {
     let vcVistasCurso = VistasViewController()
     let vcScrollView = ScrollViewController()
     let vcWebView = WebViewController()
+    let vcMapView = MapViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,24 +23,28 @@ class ViewController: UITabBarController {
         vcVistasCurso.navigationItem.largeTitleDisplayMode = .automatic
         vcScrollView.navigationItem.largeTitleDisplayMode = .automatic
         vcWebView.navigationItem.largeTitleDisplayMode = .automatic
+        vcMapView.navigationItem.largeTitleDisplayMode = .automatic
         
         let vcControlNav = UINavigationController(rootViewController: vcControlsAndViews)
         let vcVistasNav = UINavigationController(rootViewController: vcVistasCurso)
         let vcScrollNav = UINavigationController(rootViewController: vcScrollView)
         let vcWebNav = UINavigationController(rootViewController: vcWebView)
+        let vcMapNav = UINavigationController(rootViewController: vcMapView)
         
         let vcImage = UIImage(systemName: "menucard.fill")
         let vcVistaImagen = UIImage(systemName: "location.circle.fill")
         let vcScrollImagen = UIImage(systemName: "circle.fill")
         let vcWebImage = UIImage(systemName: "person")
+        let vcMapImage = UIImage(systemName: "location.fill")
         
         vcControlNav.tabBarItem  = UITabBarItem(title: "Controles", image: vcImage, tag: 1)
         vcVistasNav.tabBarItem = UITabBarItem(title: "Vistas", image: vcVistaImagen, tag: 2)
         vcScrollNav.tabBarItem = UITabBarItem(title: "Scroll", image: vcScrollImagen, tag: 3)
         vcWebNav.tabBarItem = UITabBarItem(title: "Web", image: vcWebImage, tag: 4)
+        vcMapNav.tabBarItem = UITabBarItem(title: "Map", image: vcMapImage, tag: 5)
         
         
-        let items = [vcWebNav, vcScrollNav, vcVistasNav, vcControlNav]
+        let items = [vcMapNav, vcWebNav, vcScrollNav, vcVistasNav, vcControlNav]
         
         for nav in items {
             tabBar.barTintColor = .darkGray
