5545f83ef50ccfd646f4f3286b1352e8b3b0530d
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-08-19 19:57:21 -0600

    actualizacion 2

--- CursoUndemy2024/ViewController.swift
+++ CursoUndemy2024/ViewController.swift
@@ -21,33 +21,27 @@ class ViewController: UITabBarController {
         vcControlsAndViews.navigationItem.largeTitleDisplayMode = .automatic
         vcVistasCurso.navigationItem.largeTitleDisplayMode = .automatic
         vcPalindromo.navigationItem.largeTitleDisplayMode = .automatic
+        vcScrollView.navigationItem.largeTitleDisplayMode = .automatic
+
         
         let vcControlNav = UINavigationController(rootViewController: vcControlsAndViews)
         let vcVistasNav = UINavigationController(rootViewController: vcVistasCurso)
         let vcPalindromoNav = UINavigationController(rootViewController: vcPalindromo)
-        vcScrollView.navigationItem.largeTitleDisplayMode = .automatic
-        
-        let vcControlNav = UINavigationController(rootViewController: vcControlsAndViews)
-        let vcVistasNav = UINavigationController(rootViewController: vcVistasCurso)
         let vcScrollNav = UINavigationController(rootViewController: vcScrollView)
         
         
         let vcImage = UIImage(systemName: "menucard.fill")
         let vcVistaImagen = UIImage(systemName: "location.circle.fill")
         let vcImagenPalindromo = UIImage(systemName: "circle.fill")
-        
+        let vcScrollImagen = UIImage(systemName: "circle.fill")
+
         vcControlNav.tabBarItem  = UITabBarItem(title: "Controles", image: vcImage, tag: 1)
         vcVistasNav.tabBarItem = UITabBarItem(title: "Vistas", image: vcVistaImagen, tag: 2)
         vcPalindromoNav.tabBarItem = UITabBarItem(title: "Palindormo", image: vcImagenPalindromo, tag: 3)
+        vcScrollNav.tabBarItem = UITabBarItem(title: "Scroll", image: vcScrollImagen, tag: 4)
+
         
-        let items = [vcPalindromoNav, vcVistasNav, vcControlNav]
-        let vcScrollImagen = UIImage(systemName: "circle.fill")
-        
-        vcControlNav.tabBarItem  = UITabBarItem(title: "Controles", image: vcImage, tag: 1)
-        vcVistasNav.tabBarItem = UITabBarItem(title: "Vistas", image: vcVistaImagen, tag: 2)
-        vcScrollView.tabBarItem = UITabBarItem(title: "Scroll", image: vcScrollImagen, tag: 3)
-        
-        let items = [vcScrollNav, vcVistasNav, vcControlNav]
+        let items = [vcScrollNav, vcPalindromoNav, vcVistasNav, vcControlNav]
         
         for nav in items {
             tabBar.barTintColor = .darkGray
