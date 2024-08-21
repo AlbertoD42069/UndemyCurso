6d9b2a90da44a8aa63924ed5dbd0d45395e2123b
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-08-20 22:41:36 -0600

    NavegacionTabBar

--- CursoUndemy2024/NavegacionDosViewController.swift
+++ CursoUndemy2024/NavegacionDosViewController.swift
@@ -23,8 +23,8 @@ class NavegacionDosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
-        
-        
+        let butonLeft = UIBarButtonItem(title: "rojo", style: .plain, target: self, action: #selector((goRojo)))
+        navigationItem.rightBarButtonItem = butonLeft
         /*view.addSubview(button)
         title = "verde"
 
@@ -35,12 +35,12 @@ class NavegacionDosViewController: UIViewController {
             button.heightAnchor.constraint(equalToConstant: 50),
         ])*/
     }
-   /* @objc func pushNav(){
+   @objc func goRojo(){
         print("presionado")
         let vc =  NavegacionTresViewController()
         self.navigationController?.pushViewController(vc, animated: true)
         
         
     }
-    */
+    
 }
--- CursoUndemy2024/NavegacionTresViewController.swift
+++ CursoUndemy2024/NavegacionTresViewController.swift
@@ -12,7 +12,7 @@ class NavegacionTresViewController: UIViewController {
     private let button: UIButton = {
         let btn : UIButton = UIButton(type: .system)
         btn.setTitle("blanco", for: .normal)
-        btn.addTarget(self, action: #selector(pushNav), for: .touchUpInside)
+        btn.addTarget(self, action: #selector(goWhite), for: .touchUpInside)
         btn.translatesAutoresizingMaskIntoConstraints = false
         btn.backgroundColor = .lightGray
         btn.layer.borderWidth = 1
@@ -22,10 +22,13 @@ class NavegacionTresViewController: UIViewController {
     */
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-       /* view.addSubview(button)
-        title = "rojo"
+        let butonLeft = UIBarButtonItem(title: "blanco", style: .plain, target: self, action: #selector((goWhite)))
+        navigationItem.rightBarButtonItem = butonLeft
 
+        view.backgroundColor = .red
+       //view.addSubview(button)
+        title = "rojo"
+/*
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -34,12 +37,12 @@ class NavegacionTresViewController: UIViewController {
         ])
         */
     }
-   /* @objc func pushNav(){
+   @objc func goWhite(){
         print("presionado")
         let vc =  NavegacionCuatroViewController()
         self.navigationController?.pushViewController(vc, animated: true)
         
         
-    }*/
+    }
 
 }
