f9691b6482c24c2ceca981653de6a8658e819e25
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-08-28 00:00:53 -0600

    creacion de clase de coleccion

---
+++ CursoUndemy2024/CollectionCurso/CollecctionViewCurso.swift
@@ -0,0 +1,20 @@
+//
+//  CollecctionViewCurso.swift
+//  CursoUndemy2024
+//
+//  Created by Jesus Alberto Diaz Dominguez on 28/08/24.
+//
+
+import UIKit
+
+class CollecctionViewCurso: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
